22410c10ac1bae66b0fc273cfffc46f0b9599727
Lijamaija <liza.timofeeva@gmail.com>
2020-05-01 17:31:24 +0200

    replacing classes logic with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -15,19 +15,13 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
+const App = ( { checkUserSession, currentUser }) => {
 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+useEffect(() => {
+checkUserSession();
+}, [checkUserSession]);
 
-  render() {
     return (
       <div>
         <Header />
@@ -39,7 +33,7 @@ class App extends React.Component {
             exact
             path='/signin'
             render={() =>
-              this.props.currentUser ? (
+              currentUser ? (
                 <Redirect to='/' />
               ) : (
                 <SignInAndSignUpPage />
@@ -50,15 +44,14 @@ class App extends React.Component {
       </div>
     );
   }
-}
 
-const mapStateToProps = createStructuredSelector({
+
+ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-});
-
-const mapDispatchToProps = dispatch => ({
+ });
+ const mapDispatchToProps = dispatch => ({
   checkUserSession: () => dispatch(checkUserSession())
-});
+ });
 
 export default connect(
   mapStateToProps,
